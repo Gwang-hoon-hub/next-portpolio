f0467a5986cd3e6c4d4ce015e5af93703f0bdc35
panghoon <leegwanghoon95@gmail.com>
2022-06-16 16:15:40 +0900

    [ADD] header, footer in use tailwind

--- components/layout.js
+++ components/layout.js
@@ -7,7 +7,6 @@ export default function Layout({ children }){
     return (
         <>
             <Header/>
-            <h1>레이아웃</h1>
             <div>{children}</div>
             <Footer/>
         </>
--- pages/projects.js
+++ pages/projects.js
@@ -1,9 +1,9 @@
 import Layout from "../components/layout";
 
 export default function Products(){
-return(
-        <Layout>
-            <h1>프로젝트!</h1>
-        <Layout/>
-    );
-}
+    return(
+            <Layout>
+                <h1>프로젝트!</h1>
+            </Layout>
+        );
+    }
