56bb424ef9964f318921f236b2e87790bcfc9e0e
panghoon <leegwanghoon95@gmail.com>
2022-06-18 00:32:03 +0900

    [FEAT] add DarkMode

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
+import { ThemeProvider } from 'next-themes';
 import '../styles/globals.css'
 
 // 핵심 페이지
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp
