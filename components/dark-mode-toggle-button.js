4e56d04dc86d7231ca71f7438b6a8215b4aa9642
panghoon <leegwanghoon95@gmail.com>
2022-06-18 01:05:19 +0900

    dark mode -ing

--- components/dark-mode-toggle-button.js
+++ components/dark-mode-toggle-button.js
@@ -8,11 +8,25 @@ export default function DarkModeToggleMode(){
 
     return (
         <>
-             <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0"
+             <button className="inline-flex items-center
+               border-0 py-1 px-3 rounded text-base mt-4 md:mt-0
+                focus:outline-none
+              bg-gray-300
+              hover:bg-gray-50 
+              dark:bg-slate-500
+              hover:text-orange-500
+              dark:hover:text-yellow-100
+              "
              type="button"
-             onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>Button
-                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
-                    <path d="M5 12h14M12 5l7 7-7 7"></path>
+             onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
+                <svg xmlns="http://www.w3.org/2000/svg" 
+                    className="visible dark:invisible dark:h-0 dark:w-0 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                    <path fillRule="evenodd" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" clipRule="evenodd" />
+                </svg>
+
+                {/* 다크모드 */}
+                <svg xmlns="http://www.w3.org/2000/svg" className="invisible dark:visible dark:h-5 dark:w-5 h-0 w-0" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                    <path fillRule="evenodd" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
                 </svg>
             </button>        
         </>
