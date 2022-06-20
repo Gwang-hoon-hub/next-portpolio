417a6fd2fe8c9edc9d070761950edd0991337efb
panghoon <leegwanghoon95@gmail.com>
2022-06-20 18:49:39 +0900

    ready to delpoy

--- components/projects/project-item.js
+++ components/projects/project-item.js
@@ -6,19 +6,62 @@ export default function ProjectItem({ data }) {
     const githubLing = data.properties.Github.url
     const description = data.properties.Descpription.rich_text[0].plain_text
     const imageUrl = data.cover.file?.url || data.cover.external.url
+    const tags = data.properties.Tags.multi_select
+    const start = data.properties.WorkPeriod.date.start
+    const end = data.properties.WorkPeriod.date.end
+
+    const calculatedPeriod = (start, end) => {
+        const startDateStringArray = start.split('-');
+        const endDateStringArray = end.split('-');
+        
+        var startDate = new Date(startDateStringArray[0], startDateStringArray[1], startDateStringArray[2])
+        var endDate = new Date(endDateStringArray[0], endDateStringArray[1], endDateStringArray[2])
+        
+        console.log(`startDate : ${startDate}`)
+        console.log(`endDate : ${endDate}`)
+        
+        const diffInMs = Math.abs(endDate - startDate);
+        const result = diffInMs / (1000 * 60 * 60 * 24);
+
+        console.log(`기간 : ${result}`)
+        return result;
+    }
 
     return (
-        <div className="flex flex-col p-6 m-3 bg-slate-400 rounded-md">
+        <div className="project-card">
 
             <Image
+                className="rounded-t-xl"
                 src={imageUrl}
+                alt="cover image"
                 width="100%"
                 height="60%"
-                
+                layout="responsive"
+                objectFit="cover"
+                quality={100}
             />
-            <h1>{title}</h1>
-            <a href={githubLing}> 깃허브 바로가기 </a>
-            <h2>설명 : {description}</h2>
+
+            <div className="p-4 flex flex-col">
+
+                <h1 className="text-2xl font-bold">{title}</h1>
+                <h2 className="mt-4 text-xl">설명 : {description}</h2>
+                <div className="mt-2 mb-2">
+                    <a className="px-2 py-1 mr-2 rounded-md bg-sky-200 dark:bg-yellow-500 w-30 text-black"
+                    href={githubLing}>Github Link</a>
+
+                </div>
+                <p className="my-1">
+                    기간 : {start} ~ {end} : ({calculatedPeriod(start, end)}일)
+                </p>
+                <div className="flex items-start mt-2">
+                    {tags.map((aTag) => (
+                        <h1 className="px-2 py-1 mr-2 rounded-md bg-sky-200 dark:bg-sky-700 w-30"
+                            key={aTag.id}> {aTag.name} </h1>
+                    ))}
+
+                </div>
+            </div>
+            
         </div>
     )
 }
