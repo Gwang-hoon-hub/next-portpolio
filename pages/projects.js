30343797dfc8478f9bfc0f65f8d1f270526c72ed
panghoon <leegwanghoon95@gmail.com>
2022-06-20 13:30:01 +0900

    [FEAT] : NOTION API

--- pages/projects.js
+++ pages/projects.js
@@ -1,5 +1,6 @@
 import Layout from "../components/layout";
 import Head from "next/head";
+import { TOKEN, DATABASE_ID } from "../config";
 
 export default function Products(){
     return(
@@ -13,3 +14,30 @@ export default function Products(){
             </Layout>
         );
     }
+    
+// 빌드 타임에 호출
+export async function getStaticProps() {
+
+  const options = {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Notion-Version': '2022-02-22',
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${TOKEN}`
+        },
+        body: JSON.stringify({page_size: 100})
+    };
+
+    const res = await fetch(`https://api.notion.com/v1/databases/${DATABASE_ID}/query`, options)
+
+    const projects = await res.json()
+
+    console.log(result)
+
+    const porjextIds = projects.results.map
+
+  return {
+    props: {}, // will be passed to the page component as props
+  }
+}
